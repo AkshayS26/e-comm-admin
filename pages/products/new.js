cef72568ad696915a273a54262f93a7ebb4304a9
Akshay <saawant.akshay@gmail.com>
2023-08-23 23:13:12 +0530

    first commit

---
+++ pages/products/new.js
@@ -0,0 +1,14 @@
+import Layout from "@/components/Layout";
+import ProductForm from "@/components/ProductForm";
+
+const NewProduct = () => {
+  return (
+    <>
+      <Layout>
+        <ProductForm />
+      </Layout>
+    </>
+  );
+};
+
+export default NewProduct;
